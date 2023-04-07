13eb5b56f9b998c749140954c6def261867c7df4
Savas <savastyludf@hotmail.com>
2023-04-07 05:00:01 -0400

    MFS edited

--- MillionaireFortuneteller.sh
+++ MillionaireFortuneteller.sh
@@ -5,6 +5,6 @@ echo "How old are you?"
 read age
 echo "Hello $name, you are $age years old."
 sleep 3
-getrich=$((( $RANDOM %15)+ $age))
+getrich=$((( $RANDOM %15) + $age))
 echo "$name, you will become a millionare when you are $getrich years old."
 
